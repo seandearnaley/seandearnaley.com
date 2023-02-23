69c6f23933e6a9ddeec86daa6cc8bdeb47a789c4
Sean Dearnaley <seandearnaley@hotmail.com>
2023-02-22 23:43:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,49 +1,49 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://www.seandearnaley.com',
-  pathPrefix: '/',
-  title: 'Blog by Sean Dearnaley',
-  subtitle: 'Software Engineer, Music Producer, Human Being',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://www.seandearnaley.com",
+  pathPrefix: "/",
+  title: "Blog by Sean Dearnaley",
+  subtitle: "Software (Web, AI, Mgmt), Music (Human Being), Writing (Tech)",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
   postsPerPage: 5,
-  googleAnalyticsId: 'UA-155359968-1',
+  googleAnalyticsId: "UA-155359968-1",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/",
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About me",
+      path: "/pages/about",
     },
     {
-      label: 'Music',
-      path: '/pages/human_being_music'
+      label: "Music",
+      path: "/pages/human_being_music",
     },
   ],
   author: {
-    name: 'Sean Dearnaley',
-    photo: '/photo.jpg',
-    bio: 'Software Engineer, Music Producer, Human Being',
+    name: "Sean Dearnaley",
+    photo: "/photo.jpg",
+    bio: "Software Engineer, Music Producer, Human Being",
     contacts: {
-      email: 'seandearnaley@hotmail.com',
-      facebook: '',
-      telegram: '',
-      twitter: 'seandearnaley',
-      github: 'seandearnaley',
-      rss: '',
-      vkontakte: '',
-      linkedin: 'sean-dearnaley-8577a973/',
-      instagram: '',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: 'UCSj0EB8Mn299OJEv9xzy6pg',
-      soundcloud: 'humanbeingmusic',
-    }
-  }
+      email: "seandearnaley@hotmail.com",
+      facebook: "",
+      telegram: "",
+      twitter: "seandearnaley",
+      github: "seandearnaley",
+      rss: "",
+      vkontakte: "",
+      linkedin: "sean-dearnaley-8577a973/",
+      instagram: "",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "UCSj0EB8Mn299OJEv9xzy6pg",
+      soundcloud: "humanbeingmusic",
+    },
+  },
 };
